936d33b6cbd00cc7d544951434b37b67b3969f23
nickspiess <nickspiess23@gmail.com>
2023-07-26 14:45:33 -0600

    separate quote functions

--- src/pages/quote.js
+++ src/pages/quote.js
@@ -34,134 +34,112 @@ const QuoteCard = () => {
     const loveseatsInputRef = useRef(null);
     const couchesInputRef = useRef(null);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        
-        // Check if the required fields are filled out
-        if (!firstName || !lastName || !email || !phone) {
-          alert('Please fill out all required fields.');
-          return;
-        } else {
-          const [totalPrice, quoteNumber] = quoteCalculator(
-            rooms,
-            steps,
-            chairs,
-            loveseats,
-            couches
-          );
-          const quoteData = {
-            rooms,
-            steps,
-            chairs,
-            loveseats,
-            couches,
-            firstName,
-            lastName,
-            email,
-            phone,
-            totalPrice,
-            quoteNumber,
-          };
-          const jsonQuoteData = JSON.stringify(quoteData);
-      
-          console.log('Sending quoteData to the API:', jsonQuoteData);
-      
-          try {
-            const res = await fetch('/api/sendgrid', {
-              body: JSON.stringify({
-                email: email,
-                firstName: firstName,
-                lastName: lastName,
-                roomCount: rooms,
-                stepCount: steps,
-                chairCount: chairs,
-                loveseatCount: loveseats,
-                couchCount: couches,
-                totalPrice: totalPrice,
-                quoteNumber: quoteNumber,
-                subject: 'Your Quote from Spiess Carpet!',
-                message:
-                  'Here is your quote for ' +
-                  rooms +
-                  ' rooms, ' +
-                  steps +
-                  ' flights of steps, ' +
-                  chairs +
-                  ' chairs, ' +
-                  loveseats +
-                  ' loveseats, and ' +
-                  couches +
-                  ' couches.',
-              }),
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              method: 'POST',
-            });
-      
-            console.log('API response status:', res.status);
-            console.log('API response body:', await res.text());
-      
-            if (res.ok) {
-              Swal.fire({
-                icon: 'success',
-                title: 'Message Sent Successfully',
-              });
-      
-              // Reset the form inputs
-              setRooms(0);
-              setSteps(0);
-              setChairs(0);
-              setLoveseats(0);
-              setCouches(0);
-            } else {
-              Swal.fire({
-                icon: 'error',
-                title: 'Oops, something went wrong',
-                text: await res.text(),
-              });
-            }
-      
-            // Insert the data into the database
-            try {
-              await PotentialCustomers.create(quoteData);
-              console.log('Data inserted into the database successfully.');
-            } catch (error) {
-              console.log('Database insertion error:', error);
-            }
-      
-            // Get a reference to the input element
-            const roomsInput = document.getElementById('num_input');
-      
-            // Add event listener for input change
-            roomsInput.addEventListener('input', handleInputChange);
-      
-            // Function to handle input change
-            function handleInputChange(event) {
-              const inputValue = parseInt(event.target.value);
-      
-              // Check if the input value is a number
-              if (!isNaN(inputValue)) {
-                // Check if the input value is less than 0
-                if (inputValue < 0) {
-                  // Set the input value to 0 if it is less than 0
-                  event.target.value = 0;
-                }
-              } else {
-                // Set the input value to an empty string if it is not a number
-                event.target.value = '';
-              }
-            }
-      
-            const form = document.getElementById('input'); // Replace 'yourFormId' with the actual ID of your form
-            form.reset();
-      
-            console.log(firstName, lastName, email, email, phone);
-          } catch (error) {
-            console.log(error);
-          }
-        }
-      };
+      // Function to send email using SendGrid API
+  const sendEmail = async (quoteData) => {
+    try {
+      const res = await fetch('/api/sendgrid', {
+        body: JSON.stringify({
+          email: quoteData.email,
+          firstName: quoteData.firstName,
+          lastName: quoteData.lastName,
+          roomCount: quoteData.rooms,
+          stepCount: quoteData.steps,
+          chairCount: quoteData.chairs,
+          loveseatCount: quoteData.loveseats,
+          couchCount: quoteData.couches,
+          totalPrice: quoteData.totalPrice,
+          quoteNumber: quoteData.quoteNumber,
+          subject: 'Your Quote from Spiess Carpet!',
+          message: `Here is your quote for ${quoteData.rooms} rooms, ${quoteData.steps} flights of steps, ${quoteData.chairs} chairs, ${quoteData.loveseats} loveseats, and ${quoteData.couches} couches.`,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+      });
+
+      console.log('API response status:', res.status);
+      console.log('API response body:', await res.text());
+
+      if (res.ok) {
+        Swal.fire({
+          icon: 'success',
+          title: 'Message Sent Successfully',
+        });
+        return true;
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops, something went wrong',
+          text: await res.text(),
+        });
+        return false;
+      }
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+
+  // Function to insert data into the database
+  const insertIntoDatabase = async (quoteData) => {
+    try {
+      await PotentialCustomers.create(quoteData);
+      console.log('Data inserted into the database successfully.');
+      return true;
+    } catch (error) {
+      console.log('Database insertion error:', error);
+      return false;
+    }
+  };
+
+  // The handleSubmit function with separated SendGrid and database calls
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Check if the required fields are filled out
+    if (!firstName || !lastName || !email || !phone) {
+      alert('Please fill out all required fields.');
+      return;
+    }
+
+    // Calculate total price and quote number
+    const [totalPrice, quoteNumber] = quoteCalculator(rooms, steps, chairs, loveseats, couches);
+
+    // Construct the quote data object
+    const quoteData = {
+      rooms,
+      steps,
+      chairs,
+      loveseats,
+      couches,
+      firstName,
+      lastName,
+      email,
+      phone,
+      totalPrice,
+      quoteNumber,
+    };
+
+    console.log('Sending quoteData to the API:', quoteData);
+
+    // Send email using SendGrid API
+    const emailSent = await sendEmail(quoteData);
+
+    // If email was sent successfully, insert data into the database
+    if (emailSent) {
+      const dataInserted = await insertIntoDatabase(quoteData);
+
+      // Reset the form inputs if data was inserted into the database successfully
+      if (dataInserted) {
+        setRooms(0);
+        setSteps(0);
+        setChairs(0);
+        setLoveseats(0);
+        setCouches(0);
+      }
+    }
+  };
       
 
 
