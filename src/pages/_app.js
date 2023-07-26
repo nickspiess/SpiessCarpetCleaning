9e5630bd499fa3d0fb58ba253d9321f98fd8ff4d
nickspiess <nickspiess23@gmail.com>
2023-07-26 15:02:02 -0600

    Revert "app?"
    This reverts commit 4170bc85c015fb7df6c2b7d2faa056d5795831ac.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -16,19 +16,19 @@ export default function App({ Component, pageProps }) {
 
   useEffect(() => {
     // Check if the migration has already been performed
-  //  if (!migrationPerformed) {
-  //     //Perform the migration to create the table if it doesn't exist
-  //    sequelize.sync()
-  //      .then(() => {
-  //        console.log('Database is ready.');
-  //        // Mark the migration as performed
-  //        migrationPerformed = true;
-  //      })
-  //      .catch((error) => {
-  //        console.error('Error occurred during database synchronization:', error);
-  //        // Handle the error or exit the application
-  //      });
-  //  }
+   if (!migrationPerformed) {
+      //Perform the migration to create the table if it doesn't exist
+     sequelize.sync()
+       .then(() => {
+         console.log('Database is ready.');
+         // Mark the migration as performed
+         migrationPerformed = true;
+       })
+       .catch((error) => {
+         console.error('Error occurred during database synchronization:', error);
+         // Handle the error or exit the application
+       });
+   }
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
 
