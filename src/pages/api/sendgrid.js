5625f4c8edf4d6545c6895fba59bd2e6e14463ae
nickspiess <nickspiess23@gmail.com>
2023-07-27 12:53:33 -0600

    multiple emails

--- src/pages/api/sendgrid.js
+++ src/pages/api/sendgrid.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
         try {
             await sgMail.sendMultiple({
-            to: [`${req.body.email}`, 'nickspiess23@gmail.com'], // Your email where you'll receive emails
+            to: [`${req.body.email}`, 'sales@spiesscarpet.com'], // Your email where you'll receive emails
             from: "sales@spiesscarpet.com", // your website email address here
             subject: `${req.body.subject}`,
             html: `
