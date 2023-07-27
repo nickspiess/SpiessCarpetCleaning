bffad7115e512524c3fbe0db0ae7a05fd6cdf08c
nickspiess <nickspiess23@gmail.com>
2023-07-27 13:35:31 -0600

    tracing

--- src/pages/api/submit.js
+++ src/pages/api/submit.js
@@ -1,11 +1,12 @@
 import { insertIntoDatabase } from '../../utils/databaseUtils';
 
 export default async function handler(req, res) {
+  console.log('in submit api');
   if (req.method === 'POST') {
     const quoteData = req.body;
-
+    console.log('in if');
     try {
-
+      console.log('in try');
       // Insert data into the database
       await insertIntoDatabase(quoteData);
 
