41b23de8eff262a13ac2f83a2b804e6acbfba588
nickspiess <nickspiess23@gmail.com>
2023-08-08 11:15:31 -0600

    pricing

--- src/js/quoteCalculator.js
+++ src/js/quoteCalculator.js
@@ -6,7 +6,7 @@ var chairPrice;
 var couchPrice;
 var sectionalPrice;
 
-function quoteCalculator(roomCount, numOfSteps, chairCount, couchCount, sectionalCount) {
+function quoteCalculator(roomCount, flightOfSteps, chairCount, couchCount, sectionalCount) {
     totalPrice = 0.00
     stepPrice = 0.00
     chairPrice = 0.00
@@ -16,28 +16,32 @@ function quoteCalculator(roomCount, numOfSteps, chairCount, couchCount, sectiona
     quoteNum = Math.floor(Math.random() * (200 - 1 + 1)) + 1
     // Room Pricing
     if (roomCount == 2 || roomCount == 1) {
-        totalPrice = 125.00;
+        totalPrice = 169.99;
     } else if (roomCount == 3) {
-        totalPrice = 148.00;
+        totalPrice = 224.99;
     } else if (roomCount == 4) {
-        totalPrice = 188.00;
+        totalPrice = 281.99;
     } else if (roomCount == 5) {
-        totalPrice = 233.75;
+        totalPrice = 337.99;
     } else if (roomCount == 6) {
-        totalPrice = 279.00;
+        totalPrice = 394.99;
     } else if (roomCount == 7) {
-        totalPrice = 323.75;
+        totalPrice = 449.99;
     } else if (roomCount == 8) {
-        totalPrice = 366.00;
-    } else if (roomCount > 9) {
-        totalPrice = 45.00 * roomCount;
+        totalPrice = 506.99;
+    } else if (roomCount == 8) {
+        totalPrice = 506.99;
+    } else if (roomCount == 9) {
+        totalPrice = 560.99;
+    } else if (roomCount == 10) {
+        totalPrice = 618.99;
     }
     // Sectional : $279
     // Couch/Sofa : $179.00
     // Loveseat : $139.99
     // Kitchen Chair : $19.99
     // Lazy Boy $89.99
-    totalPrice = totalPrice + (4.50 * parseFloat(numOfSteps)) + (parseFloat(chairCount) * 89.99) + (parseFloat(couchCount) * 179.00) + (parseFloat(sectionalCount) * 279.00);
+    totalPrice = totalPrice + (57.99 * parseFloat(numOfSteps)) + (parseFloat(chairCount) * 89.99) + (parseFloat(couchCount) * 179.00) + (parseFloat(sectionalCount) * 279.00);
     return [totalPrice, quoteNum];
 }
 
