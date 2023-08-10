de8f856f57e375b8f0ed5e6c763eb8e6e8d76400
nickspiess <nickspiess23@gmail.com>
2023-08-09 21:55:00 -0600

    calc

--- src/js/quoteCalculator.js
+++ src/js/quoteCalculator.js
@@ -17,31 +17,35 @@ function quoteCalculator(roomCount, flightsOfSteps, kitchenChairCount, ottomanCo
     quoteNum = Math.floor(Math.random() * (200 - 1 + 1)) + 1
     // Room Pricing
     if (roomCount == 2 || roomCount == 1) {
-        totalPrice = 125.00;
+        totalPrice = 169.99;
     } else if (roomCount == 3) {
-        totalPrice = 148.00;
+        totalPrice = 224.99;
     } else if (roomCount == 4) {
-        totalPrice = 188.00;
+        totalPrice = 281.99;
     } else if (roomCount == 5) {
-        totalPrice = 233.75;
+        totalPrice = 337.99;
     } else if (roomCount == 6) {
-        totalPrice = 279.00;
+        totalPrice = 394.99;
     } else if (roomCount == 7) {
-        totalPrice = 323.75;
+        totalPrice = 449.99;
     } else if (roomCount == 8) {
-        totalPrice = 366.00;
-    } else if (roomCount > 9) {
-        totalPrice = 45.00 * roomCount;
+        totalPrice = 506.99;
+    } else if (roomCount == 8) {
+        totalPrice = 506.99;
+    } else if (roomCount == 9) {
+        totalPrice = 560.99;
+    } else if (roomCount == 10) {
+        totalPrice = 618.99;
     }
-
-    totalPrice = totalPrice + (4.50 * parseFloat(flightsOfSteps)) + (parseFloat(lazyBoyCount) * 79.99) + 
+    console.log(totalPrice);
+    totalPrice = totalPrice + (57.99 * parseFloat(flightsOfSteps)) + (parseFloat(lazyBoyCount) * 79.99) + 
                 (parseFloat(sofaCount) * 179.00) + (parseFloat(sectionalCount) * 279.00) + (parseFloat(ottomanCount) * 24.99)
                 + (parseFloat(loveseatCount) * 159.99) + (parseFloat(kitchenChairCount) * 24.99);
-
+                console.log(totalPrice);
     if (deodorizer) {
         totalPrice = totalPrice + 40.00;
     }
-
+    console.log(totalPrice);
     return [totalPrice, quoteNum];
 }
 
