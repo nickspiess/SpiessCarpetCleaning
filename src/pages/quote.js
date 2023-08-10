a805599044e8910adc50177d38282e7cef9a83f4
nickspiess <nickspiess23@gmail.com>
2023-08-09 22:36:08 -0600

    error check

--- src/pages/quote.js
+++ src/pages/quote.js
@@ -155,7 +155,7 @@ const Quote = () => {
             if (deodorize) {
                 message = message + ' and deodorizer';
             }
-
+            try {
             const [totalPrice, quoteNumber] = quoteCalculator(
                 rooms,
                 flightsOfSteps,
@@ -166,7 +166,10 @@ const Quote = () => {
                 sofas,
                 sectionals,
                 deodorize
-            );
+            ); }
+            catch (error) {
+                console.error('Error with calculator:', error);
+              }
             
             const quoteData = {
                 rooms,
