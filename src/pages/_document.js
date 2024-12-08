c637bc9301cd100739b59db5db30f2c2c0c4fbc5
nickspiess <nickspiess23@gmail.com>
2024-12-08 14:06:43 -0700

    video and header additions

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="logo" href="favicon.ico" />
         
         {/* Google Analytics 4 */}
         <script
