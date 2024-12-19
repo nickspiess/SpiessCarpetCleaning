deb6a96360c11ae887bf1449eed20166c3786b14
nickspiess <nickspiess23@gmail.com>
2024-12-18 18:19:09 -0700

    remove special

--- src/pages/FirstLanding.jsx
+++ src/pages/FirstLanding.jsx
@@ -18,9 +18,6 @@ const FirstLanding = () => {
 
     return (
         <>
-        <div className={styles.specialsContainer}>
-        
-        </div>
             <div className={`${styles.first_container}`}>
             <header className={styles.callHeader}>
                 <h3 className={styles.h3One}><a className={styles.link} href="sms:6513238711">651-323-8711</a></h3><h3> | </h3><h3>Call or Text Today for a FREE QUOTE</h3>
